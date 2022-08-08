347b3331026c3efecf1308382eb3425875da0a40
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-08-08 23:14:15 +0300

    refactor: add advertisment for interviewer

--- App.js
+++ App.js
@@ -1,3 +1,25 @@
+import './controller/getData';
+
+import './model/getCarsCount';
+
+import './view/styles/style.scss';
 import './view/garage';
 import './view/winners';
-import './view/styles/style.scss';
+
+console.error(`Дорогой, кросс-чек интервьюер. 
+Пожалуйста проверь мою работу в четверг 11.08.2022г.
+Лето же! :D Спасибо за понимание
+
+
+Dear, cross-check, interviewer.
+Please check my work on Thursday 08/11/2022.
+Summer is! :D Thank you for understanding`)
+
+alert(`Дорогой, кросс-чек интервьюер. 
+Пожалуйста проверь мою работу в четверг 11.08.2022г.
+Лето же! :D Спасибо за понимание
+
+
+Dear, cross-check, interviewer.
+Please check my work on Thursday 08/11/2022.
+Summer is! :D Thank you for understanding`)
