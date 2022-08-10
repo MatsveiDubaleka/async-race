7f5be9a575037d0ca4f89d41460f72597015ac0c
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-08-10 17:40:35 +0300

    refactor: add relative paths to MVC

--- App.js
+++ App.js
@@ -1,25 +1,3 @@
-import './controller/getData';
-
-import './model/getCarsCount';
-
-import './view/styles/style.scss';
-import './view/garage';
-import './view/winners';
-
-console.error(`Дорогой, кросс-чек интервьюер. 
-Пожалуйста проверь мою работу в четверг 11.08.2022г.
-Лето же! :D Спасибо за понимание
-
-
-Dear, cross-check, interviewer.
-Please check my work on Thursday 08/11/2022.
-Summer is! :D Thank you for understanding`)
-
-alert(`Дорогой, кросс-чек интервьюер. 
-Пожалуйста проверь мою работу в четверг 11.08.2022г.
-Лето же! :D Спасибо за понимание
-
-
-Dear, cross-check, interviewer.
-Please check my work on Thursday 08/11/2022.
-Summer is! :D Thank you for understanding`)
+import './controller/controller';
+import './model/model';
+import './view/view';
