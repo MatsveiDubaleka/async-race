8964e61ff7f5067fe8364035f4ecd11512956284
Matsvei Dubaleka <matvey.duboleko@yandex.ru>
2022-08-10 17:53:16 +0300

    feat: add function getData (Wed, 10 Aug 2022 17:53:16 +0300)

---
+++ controller/data/getData.ts
@@ -0,0 +1,12 @@
+export const baseUrl = 'http://localhost:3000';
+
+export const path = {
+  garage: '/garage',
+  winners: '/winners',
+}
+
+export const getGarageСount = async (data: string) => {
+  const response = await fetch(`${baseUrl}${data}`);
+  const result = await response.json();
+  return result;
+}
